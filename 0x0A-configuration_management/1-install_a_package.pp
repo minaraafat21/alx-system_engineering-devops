8ab2a645c2b76f2359ced21c5ec7415937dad88c
mina <minaraafatemil14@gmail.com>
2024-02-24 21:04:34 +0200

    MON

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# install flask
+exec { 'install python packages':
+  command => 'pip3 install flask==2.1.0',
+  path    => ['/usr/bin/'],
+  unless  => '/usr/bin/test -f /usr/local/lib/python3.8/dist-packages/flask/app.py',
 }
